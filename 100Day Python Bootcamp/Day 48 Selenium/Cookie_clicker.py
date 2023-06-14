6967042f4dc3268dd38a192f6e959e2783b9f161
Helen Zheng <jzheng3@babson.edu>
2023-06-14 18:25:38 -0400

    okie It worked!!!!!!!!!!!!

--- 100Day Python Bootcamp/Day 48 Selenium/Cookie_clicker.py
+++ 100Day Python Bootcamp/Day 48 Selenium/Cookie_clicker.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
-driver.get("https://orteil.dashnet.org/cookieclicker/")
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 cookie_element = driver.find_element(By.ID, "cookie")
 loop_time = 2
