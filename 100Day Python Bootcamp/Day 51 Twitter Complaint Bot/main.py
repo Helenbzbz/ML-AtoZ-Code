cefd7e2108a657a18c8a95f8f4ba52f29af47f16
Helen Zheng <jzheng3@babson.edu>
2023-06-15 13:40:06 -0400

    start

---
+++ 100Day Python Bootcamp/Day 51 Twitter Complaint Bot/main.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+PROMISED_DOWN = 150
+PROMISED_UP = 10
+TWITTER_ACCOUNT = "AnnaLee1656639"
+TWITTER_PWD = "878788609"
+
+class InternetSpeedTwitterBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.down = 0
+        self.up = 0
+
+    def get_internet_speed():
+        pass
+
+    def tweet_at_provider():
+        pass
