e9df7e615be9ee6e3e9ed6f95a186ab209c1e790
Helen Zheng <jzheng3@babson.edu>
2023-06-24 19:33:00 -0400

    python

--- 100Day Python Bootcamp/Day 65 Create API/main.py
+++ 100Day Python Bootcamp/Day 65 Create API/main.py
@@ -4,6 +4,8 @@ import random
 
 app = Flask(__name__)
 
+API = "helenzhengbzbz9120884"
+
 ##Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -60,7 +62,9 @@ def str_to_bool(arg_from_url):
     else:
         return False
 
-@app.route("/add", methods = ['POST'])
+
+## HTTP POST - Create Record
+@app.route("/add", methods = ["GET", 'POST'])
 def add():
     new_cafe = Cafe(
         name = request.form.get("name"),
@@ -68,22 +72,45 @@ def add():
         img_url = request.form.get("img_url"),
         location = request.form.get("location"),
         seats = request.form.get("seats"),
-        has_toilet = request.form.get("has_toilet"),
-        has_wifi = request.form.get("has_wifi"),
-        has_sockets = request.form.get("has_sockets"),
-        can_take_calls = request.form.get("can_take_calls"),
+        has_toilet = str_to_bool(request.form.get("has_toilet")),
+        has_wifi = str_to_bool(request.form.get("has_wifi")),
+        has_sockets = str_to_bool(request.form.get("has_sockets")),
+        can_take_calls = str_to_bool(request.form.get("can_take_calls")),
         coffee_price = request.form.get("coffee_price")
         )
     db.session.add(new_cafe)
     db.session.commit()
     return jsonify(response = {'success':'Successfully added the new cafe'})
 
-## HTTP POST - Create Record
 
 ## HTTP PUT/PATCH - Update Record
+@app.route("/update-price/<int:cafe_id>", methods = ["GET", "PATCH"])
+def update(cafe_id):
+    with app.app_context():
+        new_price = request.args.get("new_price")
+        cafe = db.session.query(Cafe).get(cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."})
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
 
 ## HTTP DELETE - Delete Record
-
+@app.route("/report-closed/<int:cafe_id>", methods = ["GET", "DELETE"])
+def delete(cafe_id):
+    api_key = request.args.get("api")
+    if api_key == API:
+        with app.app_context():
+            cafe = db.session.query(Cafe).get(cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully updated the price."})
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+    else:
+        return jsonify(error={"API Error": "Sorry the API Key is wrong."})
 
 if __name__ == '__main__':
     app.run(debug=True)
